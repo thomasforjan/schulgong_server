205cb634b2ae8b6d663d99b886f55b756fef7f6c
Philipp Wildzeiss <philipp.wildzeiss@aon.at>
2023-04-02 23:58:50 +0200

    add: Setup Spring Application
    Setup Spring application with Gradle.
    Setup project structure.
    
    SCHUL-14

---
+++ backend/src/main/java/at/schulgong/BackendApplication.java
@@ -0,0 +1,9 @@
+package at.schulgong;
+
+import org.springframework.boot.SpringApplication;
+
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
---
+++ speaker-api/src/main/java/at/schulgong/SpeakerApiApplication.java
@@ -0,0 +1,9 @@
+package at.schulgong;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpeakerApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpeakerApiApplication.class, args);
+    }
+}
