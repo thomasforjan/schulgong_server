109ca03491bbb59c6703a5ed2d0fa322e9811fda
Martin Kral <2110859031@fh-burgenland.at>
2023-04-05 13:24:12 +0200

    fix: Annotation in executable classes
    @SpringBootApplication-annotation has been missing in both classes

--- speaker-api/src/main/java/at/schulgong/SpeakerApiApplication.java
+++ speaker-api/src/main/java/at/schulgong/SpeakerApiApplication.java
@@ -1,7 +1,9 @@
 package at.schulgong;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpeakerApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpeakerApiApplication.class, args);
