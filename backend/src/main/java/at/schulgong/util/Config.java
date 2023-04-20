830021f2836f94d80a0b85d8a820b9ffbc94b25b
Martin Kral <2110859031@fh-burgenland.at>
2023-04-21 00:11:00 +0200

    add: Config for global configuration
    The class Config contains global configurations e.g. url, path, ...

---
+++ backend/src/main/java/at/schulgong/util/Config.java
@@ -0,0 +1,26 @@
+package at.schulgong.util;
+
+import lombok.Getter;
+
+/**
+ * @author Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version 0.1
+ * @implNote Config-Enum to configure global values
+ * @since April 2023
+ */
+@Getter
+public enum Config {
+
+  RINGTONE("ringtones", "", "ringtone"),
+  FILEPATH("", "C:/Users/kralm/Desktop/Audiofiles", "");
+
+  private final String url, path, exception;
+
+  Config(String url, String path, String exception) {
+    this.url = url;
+    this.path = path;
+    this.exception = exception;
+  }
+}
+
+
