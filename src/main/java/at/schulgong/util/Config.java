5e33c23f4ec4c634478f5246049ce39287358701
Martin Kral <2110859031@fh-burgenland.at>
2023-05-11 16:57:05 +0200

    refactor: Config
    Refactored config-file, updated javadoc

--- src/main/java/at/schulgong/util/Config.java
+++ src/main/java/at/schulgong/util/Config.java
@@ -4,20 +4,30 @@ import lombok.Getter;
 
 /**
  * @author Thomas Forjan, Philipp Wildzeiss, Martin Kral
- * @version 0.1
+ * @version 0.2
  * @implNote Config-Enum to configure global values
  * @since April 2023
  */
 @Getter
 public enum Config {
-    RINGTONE("ringtones", "", "ringtone"),
-    HOLIDAY("holidays", "", "holiday"),
-    FILEPATH("", "C:/Users/rayle/Desktop/Audiofiles", "");
-    private final String url, path, exception;
 
-    Config(String url, String path, String exception) {
-        this.url = url;
-        this.path = path;
-        this.exception = exception;
-    }
+  RINGTONE("ringtones", "", "ringtone"),
+  RINGTIME("ringtimes", "", "ringtime"),
+  HOLIDAY("holidays", "", "holiday"),
+  FILEPATH("", "C:/Users/kralm/Desktop/Audiofiles", "");
+
+  private final String url, path, exception;
+
+  /**
+   * Constructor of enum
+   *
+   * @param url       url for CRUD
+   * @param path      path for file saving
+   * @param exception name of entity for exception
+   */
+  Config(String url, String path, String exception) {
+    this.url = url;
+    this.path = path;
+    this.exception = exception;
+  }
 }
