095c6322aebe9c1d5be605206777beb64987809e
Philipp Wildzeiss <philipp.wildzeiss@aon.at>
2023-05-21 16:22:34 +0200

    add: Functionality for playing an announcement
    Add a flag to PlayRingtones that only if it is not true, ringtimes can be loaded and played.
    Add a play, execute, and run actions after announcement method for the playing announcement.

--- src/main/java/at/schulgong/util/Config.java
+++ src/main/java/at/schulgong/util/Config.java
@@ -14,7 +14,8 @@ public enum Config {
     RINGTIME("ringtimes", "", "ringtime"),
     HOLIDAY("holidays", "", "holiday"),
     SPEAKER_API_SETTINGS_PATH("", "src/main/resources/python/config/speaker_api_settings.json", ""),
-    FILEPATH("", "Audiofiles", "");
+    FILEPATH("", "Audiofiles", ""),
+    ANNOUNCEMENT_PATH("", "C:/Users/phili/OneDrive/Desktop/sonos_music/Durchsage.mp3", "");
 
     private final String url;
     private final String path;
