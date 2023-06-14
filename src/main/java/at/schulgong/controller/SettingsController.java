457e3a6376975b2fccef3841ee27807336e4fb88
Thomas Forjan <112717699+thomasforjan@users.noreply.github.com>
2023-06-14 17:41:34 +0200

    add: Change password functionality
    This commit adds a new POST mapping which allows to change the password.
    
    SCHUL-236

--- src/main/java/at/schulgong/controller/SettingsController.java
+++ src/main/java/at/schulgong/controller/SettingsController.java
@@ -4,8 +4,14 @@ import at.schulgong.dto.ConfigurationDTO;
 import at.schulgong.speaker.api.PlayRingtones;
 import at.schulgong.util.Config;
 import at.schulgong.util.ReadWriteConfigurationFile;
+import at.schulgong.util.SecretKeyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 /**
@@ -18,7 +24,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("settings")
 @CrossOrigin
 public class SettingsController {
-
+  Argon2PasswordEncoder argon2PasswordEncoder = new Argon2PasswordEncoder(16, 32, 1, 60000, 10);
   private final PlayRingtones playRingtones;
 
   /**
@@ -46,7 +52,7 @@ public class SettingsController {
   @PostMapping("volume/ringtime/{volume}")
   public ResponseEntity saveRingtimeVolume(@PathVariable int volume) {
     ConfigurationDTO configurationDTO = ReadWriteConfigurationFile.readConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath());
-    if(configurationDTO != null) {
+    if (configurationDTO != null) {
       configurationDTO.setRingtimeVolume(volume);
       try {
         ReadWriteConfigurationFile.writeConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath(), configurationDTO);
@@ -54,7 +60,7 @@ public class SettingsController {
       } catch (RuntimeException e) {
         ResponseEntity.internalServerError().build();
       }
-    }else {
+    } else {
       ResponseEntity.internalServerError().build();
     }
     return ResponseEntity.ok().build();
@@ -68,7 +74,7 @@ public class SettingsController {
   @PostMapping("volume/alarm/{volume}")
   public ResponseEntity saveAlarmVolume(@PathVariable int volume) {
     ConfigurationDTO configurationDTO = ReadWriteConfigurationFile.readConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath());
-    if(configurationDTO != null) {
+    if (configurationDTO != null) {
       configurationDTO.setAlarmVolume(volume);
       try {
         ReadWriteConfigurationFile.writeConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath(), configurationDTO);
@@ -76,7 +82,7 @@ public class SettingsController {
       } catch (RuntimeException e) {
         ResponseEntity.internalServerError().build();
       }
-    }else {
+    } else {
       ResponseEntity.internalServerError().build();
     }
     return ResponseEntity.ok().build();
@@ -90,7 +96,7 @@ public class SettingsController {
   @PostMapping("volume/announcement/{volume}")
   public ResponseEntity saveAnnouncementVolume(@PathVariable int volume) {
     ConfigurationDTO configurationDTO = ReadWriteConfigurationFile.readConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath());
-    if(configurationDTO != null) {
+    if (configurationDTO != null) {
       configurationDTO.setAnnouncementVolume(volume);
       try {
         ReadWriteConfigurationFile.writeConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath(), configurationDTO);
@@ -98,10 +104,55 @@ public class SettingsController {
       } catch (RuntimeException e) {
         ResponseEntity.internalServerError().build();
       }
-    }else {
+    } else {
       ResponseEntity.internalServerError().build();
     }
     return ResponseEntity.ok().build();
   }
 
+  /**
+   * POST request to update the password
+   *
+   * @param payload Map with the current password, the new password and the confirm new password
+   * @return ResponseEntity with the status code and a message
+   */
+  @PostMapping("/updatePassword")
+  public ResponseEntity updatePassword(@RequestBody Map<String, String> payload) {
+    String currentPassword = payload.get("currentPassword");
+    String newPassword = payload.get("newPassword");
+    String confirmNewPassword = payload.get("confirmNewPassword");
+
+    ConfigurationDTO configurationDTO = ReadWriteConfigurationFile.readConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath());
+
+    // Check if the current password is correct
+    if (configurationDTO != null && !argon2PasswordEncoder.matches(currentPassword, configurationDTO.getPassword())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Aktuelles Passwort ist falsch!");
+    }
+
+    // Check if the new passwort is equal to the confirm new password
+    if (!newPassword.equals(confirmNewPassword)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Passwörter stimmen nicht überein!");
+    }
+
+    // Check if the new password is at least 6 characters long
+    if (newPassword.length() < 6) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Passwort muss mindestens 6 Zeichen lang sein.");
+    }
+
+    // Continue with updating the password
+    if (configurationDTO != null) {
+      String hashedPassword = argon2PasswordEncoder.encode(newPassword);
+      configurationDTO.setPassword(hashedPassword);
+
+      try {
+        ReadWriteConfigurationFile.writeConfigurationDTOFromConfigFile(Config.CONFIGURATION_PATH.getPath(), configurationDTO);
+      } catch (RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+      }
+    } else {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    return ResponseEntity.status(200).body("Passwort wurde erfolgreich aktualisiert!");
+  }
 }
