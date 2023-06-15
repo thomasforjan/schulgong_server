c6e1cb701f746690be5e2f9124c3dc09d6fc7c2c
Philipp Wildzeiss <philipp.wildzeiss@aon.at>
2023-06-15 18:07:55 +0200

    update: Adjust reading json files

--- src/main/java/at/schulgong/speaker/util/ReadSettingFile.java
+++ src/main/java/at/schulgong/speaker/util/ReadSettingFile.java
@@ -22,37 +22,12 @@ public class ReadSettingFile {
      *
      * @return settings
      */
-    public static Setting getSettingFromConfigFile2() {
+    public static Setting getSettingFromConfigFile() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         try {
             Path path = Paths.get(Config.SPEAKER_API_SETTINGS_PATH.getPath());
             return objectMapper.readValue(new File(path.toAbsolutePath().toUri()), Setting.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new Setting.SettingBuilder()
-                    .type("python")
-                    .executedFilePath("python/config/main_sonos_api.py")
-                    .loadRingtimeTime(LocalTime.of(0, 1))
-                    .build();
-        }
-    }
-
-    public static Setting getSettingFromConfigFile() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        String contents;
-        try {
-            Path path = Paths.get(Config.SPEAKER_API_SETTINGS_PATH.getPath());
-            try (InputStream inputStream =
-                            ReadSettingFile.class
-                                    .getClassLoader()
-                                    .getResourceAsStream("speaker_api_settings.json");
-                    BufferedReader reader =
-                            new BufferedReader(new InputStreamReader(inputStream))) {
-                contents = reader.lines().collect(Collectors.joining(System.lineSeparator()));
-            }
-            return objectMapper.readValue(contents, Setting.class);
         } catch (IOException e) {
             e.printStackTrace();
             return new Setting.SettingBuilder()
